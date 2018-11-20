43adf99ed3b88d3160f963ab1579acd3cd924684
Adrian Frith <adrian@adrianfrith.com>
2018-11-20 23:36:52 +0200

    Add aerial view option to map

--- src/static/map.js
+++ src/static/map.js
@@ -1,9 +1,20 @@
 var map = L.map('place-map');
 
-L.tileLayer(
+var osm = L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'}
-).addTo(map);
+);
+osm.addTo(map);
+
+var ngi = L.tileLayer(
+  'http://{s}.aerial.openstreetmap.org.za/ngi-aerial/{z}/{x}/{y}.jpg',
+  {attribution: 'Aerial photo by <a href="http://www.ngi.gov.za/">CD:NGI</a>'}
+);
+
+L.control.layers({
+  'Map': osm,
+  'Satellite': ngi
+}).addTo(map);
 
 window.fetch('/place/' + placecode + '/geom').then(function (response) {
   return response.json();
